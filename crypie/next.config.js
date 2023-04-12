362c6d37d0205d46682b345445e6657b06639a60
SoniaGrassia <grassiasonia@gmail.com>
2023-04-12 16:02:59 +0200

    crypie

--- crypie/next.config.js
+++ crypie/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  sassOptions: {
+    includePaths: ["src/styles"],
+    prependData: `
+    @import "./scss/mixins/_flex.scss",
+    @import "./scss/mixins/_size.scss",
+    @import "./scss/variables/_colors.scss",
+    @import "./scss/variables/_typography.scss",
 
+
+    `,
+  },
+};
 module.exports = nextConfig;
