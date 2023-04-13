28e286319c4a75b986a3523ea22e6a464201d49c
Franceschio <coppolaciccio1@gmail.com>
2023-04-13 10:42:09 +0200

    prova git

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,11 @@
 import styles from "./index.module.scss";
 
 const Footer = () => {
-  return <div className={styles.Footer}></div>;
+  return (
+    <div className={styles.Footer}>
+      <h1>Footer</h1>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Layout from "@/components/layout";
 
 export default function Home() {
   return (
@@ -10,6 +11,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.Main}>
+        <Layout>
+          <h1>Main</h1>
+        </Layout>
+      </main>
     </>
   );
 }
