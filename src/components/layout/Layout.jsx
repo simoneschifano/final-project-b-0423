60823644283ffa6608e2eae0acf0464ef80421ba
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-04-13 13:41:04 +0200

    sidebar

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,12 +4,15 @@ import Navbar from "../navbar";
 import Header from "../header";
 
 const Layout = ({ children }) => {
+  
   return (
-    <div className={styles.Layout}>
+    
+    <div className={styles.Layout} >
       <Header />
       <Navbar />
       {children}
-    </div>
+      </div>
+      
   );
 };
 
