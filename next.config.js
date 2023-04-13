ecf572d94395df5c782c4dc817c76ac15084879f
SoniaGrassia <grassiasonia@gmail.com>
2023-04-13 10:50:29 +0200

    variables colours

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
   sassOptions: {
     includePaths: ["scss"],
     prependData: `
@@ -13,7 +12,5 @@ const nextConfig = {
 
     `,
   },
-=======
->>>>>>> 7d0bd0b76807e119db40854c04e9db6f6b95b46a
 };
 module.exports = nextConfig;
