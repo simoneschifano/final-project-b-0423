4ca4ee61a785fddbbc527678aaf6564379d28008
Franceschio <coppolaciccio1@gmail.com>
2023-04-13 18:53:47 +0200

    Modifiche alla card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,29 @@
 import styles from "./index.module.scss";
 
+import { MdStars } from "react-icons/Md";
+
 const Card = (props) => {
   const data = props;
   const coins = data.props;
   return (
     <div className={styles.Card}>
       <div className={styles.details}>
-        <div className={styles.actions}></div>
-        <div className={styles.graph}></div>
+        <div className={styles.actions}>
+          <div className={styles.icons}>
+            <MdStars className={styles.icon} />
+            <img src={coins.image} alt={coins.id} />
+          </div>
+          <h1 className={styles.symbol}>{coins.symbol.toUpperCase()}</h1>
+        </div>
       </div>
-      <div className={styles.info}>{coins.id}</div>
+      <div className={styles.graph}></div>
+      <div className={styles.stat}>
+        <span className={styles.price}>€ {coins.current_price}</span>
+        <span className={styles.var}>
+          {coins.price_change_percentage_24h.toFixed(2)}%
+        </span>
+      </div>
+      <div className={styles.info}></div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ data }) {
 
 export async function getStaticProps() {
   const res = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
   );
   const data = await res.json();
 
