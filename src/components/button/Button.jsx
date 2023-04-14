e8ef9d7cabebf83c25871d3f41ef9bd17c98a924
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-04-14 08:41:29 +0200

    login page

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,10 @@
 import styles from "./index.module.scss";
 
 const Button = () => {
-  return <div className={styles.Button}></div>;
+  return <div className={styles.Button}> Accedi <svg xmlns="http://www.w3.org/2000/svg" width="16.588" height="10.373" viewBox="0 0 16.588 10.373">
+  <path id="arrow-left-long" d="M.3,4.454a1.038,1.038,0,0,0,0,1.468l4.147,4.147A1.038,1.038,0,0,0,5.919,8.6L3.541,6.223H15.551a1.037,1.037,0,1,0,0-2.074H3.541L5.919,1.771A1.038,1.038,0,0,0,4.451.3L.3,4.451Z" transform="translate(16.588 10.373) rotate(180)"/>
+</svg>
+</div>;
 };
 
 export default Button;
