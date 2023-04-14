120d49aeeb338db8eb175f2d0e114d4eb2572e14
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-04-14 19:57:51 +0200

    card optimization scss mixin

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -17,7 +17,7 @@ const Card = (props) => {
     <div className={styles.Card} onClick={onHandleOpenDetails}>
       <div className={styles.details}>
         <div className={styles.actions}>
-          <p className={styles.rank}>{coins.market_cap_rank}.</p>
+          <p className={styles.rank}>{coins.market_cap_rank}</p>
           <img className={styles.image} src={coins.image} alt={coins.id} />
           <div className={styles.icons}>
             <h1 className={styles.symbol}>{coins.symbol.toUpperCase()}</h1>
@@ -31,25 +31,25 @@ const Card = (props) => {
 
       <div className={styles.stat}>
         <MdStars className={styles.icon} />
-        <span className={styles.price}>€{coins.current_price}</span>
-        <span className={styles.var}>
+        <p className={styles.price}>€{coins.current_price}</p>
+        <p className={styles.var}>
           {coins.price_change_percentage_24h.toFixed(2)}%
-        </span>
+        </p>
       </div>
       <div className={styles.info}>
         <div className={styles.day}>
-          <span>{coins.symbol.toUpperCase()}-NOW</span>
-          <span className={styles.digits}>€{coins.current_price}</span>
+          <p>{coins.symbol.toUpperCase()}-NOW</p>
+          <p className={styles.digits}>€{coins.current_price}</p>
         </div>
         <div className={styles.week}>
-          <span>{coins.symbol.toUpperCase()}-CHANGE PERC.</span>
-          <span className={styles.digits}>
+          <p>{coins.symbol.toUpperCase()}-CHANGE PERC.</p>
+          <p className={styles.digits}>
             {coins.price_change_percentage_24h.toFixed(2)}%
-          </span>
+          </p>
         </div>
         <div className={styles.month}>
-          <span>{coins.symbol.toUpperCase()}-TODAY</span>
-          <span className={styles.digits}>€{coins.price_change_24h}</span>
+          <p>{coins.symbol.toUpperCase()}-TODAY</p>
+          <p className={styles.digits}>€{coins.price_change_24h}</p>
         </div>
       </div>
     </div>
