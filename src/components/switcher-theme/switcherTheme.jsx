5bdf2c708e6b00d83bda96c5323af4fbde3f306b
Franceschio <coppolaciccio1@gmail.com>
2023-04-14 22:15:13 +0200

    modificate card in mobile

--- src/components/switcher-theme/switcherTheme.jsx
+++ src/components/switcher-theme/switcherTheme.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import styles from "./index.module.scss";
 
+import { BsMoon, BsSun } from "react-icons/bs";
+
 const SwitcherTheme = () => {
-  return <div className={styles.SwictherTheme}></div>;
+  const [isLightActive, setLightActive] = useState(false);
+
+  const onHandleClick = () => setLightActive((prev) => !prev);
+  return (
+    <div onClick={onHandleClick} className={styles.SwictherTheme}>
+      <div className={`${styles.dark} ${isLightActive && styles.unactive}`}>
+        <BsMoon className={styles.moon} />
+      </div>
+      <div className={`${styles.light} ${isLightActive && styles.active}`}>
+        <BsSun className={styles.sun} />
+      </div>
+    </div>
+  );
 };
 
 export default SwitcherTheme;
