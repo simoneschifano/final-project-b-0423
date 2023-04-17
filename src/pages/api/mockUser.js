ce370c6f1870aae933b2e21ab90f77cb4ecf485e
SoniaGrassia <grassiasonia@gmail.com>
2023-04-17 15:04:43 +0200

    credit card

--- src/pages/api/mockUser.js
+++ src/pages/api/mockUser.js
@@ -1,5 +1,10 @@
 export const mockUser = {
-  username: "Sonia",
-  email: "sonia@prova.it",
+  username: "Karl1994",
+  email: "karldoe@example.it",
   password: "12345",
+  cardNum: "123456789123",
+  expCard: "21/04",
+  secretNum: "420",
+  name: "Karl",
+  surname: "Doe",
 };
