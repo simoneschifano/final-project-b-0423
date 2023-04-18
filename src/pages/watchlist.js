f887644ce362110548cb99325d186d23f648caff
Vincenzo Costantino <vincenzo.costantino04@gmail.com>
2023-04-18 16:25:05 +0200

    watchlist

--- src/pages/watchlist.js
+++ src/pages/watchlist.js
@@ -1,9 +1,41 @@
 import Head from "next/head";
 import Layout from "@/components/layout";
+import { useEffect, useState } from "react";
+import CardsList from "@/components/cards_list";
+import { cryptoJson } from "./api/crypto";
 
 
 export default function watchlist() {
   const mode = "dark_mode";
+  const [coin, setCoin] = useState("");
+  const [allCrypto, setAllCrypto] = useState([]);
+  console.log(allCrypto)
+  useEffect(() => {
+    const storedName = JSON.parse(localStorage.getItem("watchlist"));
+    if (storedName) {
+      setCoin(storedName);
+    }
+  }, []);
+  useEffect(() => {
+    fetch(
+      process.env.NEXT_PUBLIC_API_URL +
+        "markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+    )
+      .then((res) => res.json())
+      .then((data) => setAllCrypto(data));
+  }, []);
+  const GetCoins=() => {
+    const stars=[];
+    if (coin && allCrypto){coin.forEach(item => {
+      console.log(allCrypto.includes(e=>e.id===item))  
+      
+    })}
+    return(
+      <div>pippo</div>
+    )
+  }
+  
+
   return (
     <>
       <Head>
@@ -15,6 +47,10 @@ export default function watchlist() {
       <main className={mode}>
         <Layout>
           <h2>  watchlist </h2>
+          <div>
+            {GetCoins()}
+            <CardsList data={allCrypto} />
+         </div>
         </Layout>
       </main>
     </>
