ee1168b408221a78b0404593ee955305dee093ef
Vincenzo Costantino <vincenzo.costantino04@gmail.com>
2023-04-19 12:41:58 +0200

    watchlist

--- src/pages/watchlist.js
+++ src/pages/watchlist.js
@@ -2,42 +2,32 @@ import Head from "next/head";
 import Layout from "@/components/layout";
 import { useEffect, useState } from "react";
 import CardsList from "@/components/cards_list";
-import { cryptoJson } from "./api/crypto";
-
 
 export default function watchlist() {
   const mode = "dark_mode";
-  // const [coin, setCoin] = useState("");
-  // const [allCrypto, setAllCrypto] = useState([]);
+  const [allCrypto, setAllCrypto] = useState([]);
 
-  // useEffect(() => {
-  //   const storedName = JSON.parse(localStorage.getItem("watchlist"));
-  //   if (storedName) {
-  //     setCoin(storedName);
-  //   }
-  // }, []);
+  const [coin, setCoin] = useState(
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("watchlist"))
+      : []
+  );
+  console.log(coin)
+  useEffect(() => {
+    fetch(
+      process.env.NEXT_PUBLIC_API_URL +
+        "markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+    )
+      .then((res) => res.json())
+      .then((data) => setAllCrypto(data));
+  }, []);
 
-  // useEffect(() => {
-  //   fetch(
-  //     process.env.NEXT_PUBLIC_API_URL +
-  //       "markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setAllCrypto(data));
-  // }, []);
-
-  // let stars=[];
-  // const GetCoins=() => {
-  //   const stars=[];
-  //   if (coin && allCrypto){coin.forEach(item => {
-  //     console.log(allCrypto.includes(e=>e.id===item))  
-      
-  //   })}
-  //   return(
-  //     <div>pippo</div>
-  //   )
-  // }
-  
+    let stars=[];
+    if (coin.length > 0) {
+      stars = allCrypto.filter((crypto) =>
+      coin.includes(crypto.id)
+      );
+    }
 
   return (
     <>
@@ -50,10 +40,9 @@ export default function watchlist() {
       <main className={mode}>
         <Layout>
           <h2>  watchlist </h2>
-          {/* <div>
-            {GetCoins()}
+          <div>
             <CardsList data={stars} />
-         </div> */}
+         </div>
         </Layout>
       </main>
     </>
