aaa473e7b2acf07f242781941c4de006287cbefc
Elisabetta <elisabetta.monaco@publicissapient.com>
2023-04-19 16:26:41 +0200

    remove console.log

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,10 +5,7 @@ import Header from "../header";
 import { useState } from "react";
 
 const Layout = ({ children, theme }) => {
-  //const [isSwitcherTheme, setIsSwitcherTheme] = useState(theme);
   
-  //const DL = isSwitcherTheme ? styles.dark : styles.unactive;
-  console.log(theme)
   return (
     <div className={styles.Layout}>
       <div className={theme ? styles.dark : styles.unactive}>
