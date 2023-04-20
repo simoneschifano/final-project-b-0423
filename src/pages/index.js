831778446413e6d5956087ee84a755dc1b513f80
Vincenzo Costantino <vincenzo.costantino04@gmail.com>
2023-04-20 08:32:18 +0200

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -104,7 +104,7 @@ export default function Home() {
                   sectionWallet && styles.sectionContActive
                 }`}
               >
-                {walletHome ? (
+                {walletHome>0 ? (
                   <CardsList data={walletHome} inHomeActive={true} />
                 ) : (
                   <h2>You don't have any elements in your wallet.</h2>
@@ -135,7 +135,7 @@ export default function Home() {
                   sectionWatchlist && styles.sectionContActive
                 }`}
               >
-                {watchlistHome ? (
+                {watchlistHome>0 ? (
                   <CardsList data={watchlistHome} inHomeActive={true} />
                 ) : (
                   <h2>You don't have any elements in your watchlist.</h2>
