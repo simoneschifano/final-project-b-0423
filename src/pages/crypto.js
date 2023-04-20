2755d6f8b93891fd50b99988a3afb7d926b3cc48
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-04-20 21:33:00 +0200

    crypto page data fixed

--- src/pages/crypto.js
+++ src/pages/crypto.js
@@ -1,6 +1,6 @@
 import Layout from "@/components/layout";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { GET } from "@/utils/http";
 import ChartEl from "@/components/chartEl";
 import styles from "@/styles/pages/crypto.module.scss";
@@ -16,7 +16,7 @@ export default function cryptoId() {
   const { name } = router.query;
   const [singleCryptoData, setSingleCryptoData] = useState([]);
   const [isGlobalModal, setIsGlobalModal] = useState(false);
-  const [cryptoInfo, setCryptoInfo] = useState([]);
+  const [cryptoInfo, setCryptoInfo] = useState(null);
 
   if (typeof window !== "undefined") {
     if (localStorage.getItem("watchlist")) {
@@ -25,7 +25,6 @@ export default function cryptoId() {
       localStorage.setItem("watchlist", "[]");
     }
   }
-  console;
 
   const [watchlist, setWatchlist] = useState(
     typeof window !== "undefined"
@@ -40,15 +39,17 @@ export default function cryptoId() {
       GET(`${name}/market_chart?vs_currency=eur&days=7&interval=daily`).then(
         (data) => setSingleCryptoData(data.prices)
       );
-      GET(`${name}`).then((data) => setCryptoInfo(() => data));
+      // GET(`${name}`).then((data) => setCryptoInfo(() => data));
     }
   }, [router.isReady]);
+
+  useEffect(() => {
+    GET(`${name}`).then((data) => setCryptoInfo(() => data));
+  }, []);
 
   const onHandleOpenModal = () => {
     setIsGlobalModal((prev) => !prev);
   };
-
-  console.log(cryptoInfo);
 
   const onHandleStar = () => {
     if (typeof window !== "undefined") {
@@ -85,11 +86,11 @@ export default function cryptoId() {
         <div className={styles.header}>
           <div className={styles.row}>
             <div className={styles.col}>
-              <h2> {cryptoInfo.market_cap_rank}</h2>
+              <h2> {cryptoInfo?.market_cap_rank}</h2>
               <img
                 className={styles.image}
-                src={cryptoInfo.image && cryptoInfo.image.large}
-                alt={cryptoInfo.name}
+                src={cryptoInfo?.image && cryptoInfo.image.large}
+                alt={cryptoInfo?.name}
               />
               <h2>{name}</h2>
               <MdStars
@@ -112,78 +113,63 @@ export default function cryptoId() {
           <ChartEl prices={singleCryptoData} />
         </div>
 
-        <div className={styles.info}>
-          <div className={styles.price}>
-            <div className={styles.priceDettails}>
-              <h5 className={styles.priceValue}>
-                <span>Price: </span> €
-                {cryptoInfo.market_data &&
-                  cryptoInfo.market_data.current_price.eur}
-              </h5>
-              <h6
-                className={
-                  cryptoInfo.market_data &&
-                  cryptoInfo.market_data.current_price
-                    .price_change_percentage_24h_in_currency &&
-                  cryptoInfo.market_data.current_price
-                    .price_change_percentage_24h_in_currency.eur > 0
-                    ? styles.positiveVar
-                    : styles.negativeVar
-                }
-              >
-                {cryptoInfo.market_data &&
-                  cryptoInfo.market_data.current_price
-                    .price_change_percentage_24h_in_currency &&
-                  cryptoInfo.market_data.current_price
-                    .price_change_percentage_24h_in_currency.eur}
-              </h6>
+        {cryptoInfo?.market_data && (
+          <>
+            <div className={styles.info}>
+              <div className={styles.price}>
+                <div className={styles.priceDettails}>
+                  <h5 className={styles.priceValue}>
+                    <span>Price: </span> €
+                    {cryptoInfo?.market_data?.current_price?.eur}
+                  </h5>
+                  <h6
+                    className={
+                      cryptoInfo?.market_data?.price_change_percentage_24h > 0
+                        ? styles.positiveVar
+                        : styles.negativeVar
+                    }
+                  >
+                    {cryptoInfo?.market_data?.price_change_percentage_24h.toFixed(
+                      2
+                    )}
+                    %
+                  </h6>
+                </div>
+
+                <div className={styles.HLprice}>
+                  <h6>
+                    Lowest:
+                    {cryptoInfo?.market_data?.current_price?.low_24h?.eur}€
+                  </h6>
+                  <h6>
+                    Hightest:
+                    {cryptoInfo?.market_data?.high_24h.eur}€
+                  </h6>
+                </div>
+              </div>
+
+              <div className={styles.supply}>
+                <h6>
+                  Circulating supply:
+                  {cryptoInfo?.market_data?.circulating_supply}
+                </h6>
+                <h6>
+                  Total supply:
+                  {cryptoInfo.market_data &&
+                    cryptoInfo.market_data.current_price.total_supply}
+                </h6>
+              </div>
+
+              <div className={styles.marketCap}>
+                <h6>Market Cap: €{cryptoInfo.market_data?.market_cap?.eur}</h6>
+              </div>
             </div>
-
-            <div className={styles.HLprice}>
-              <h6>
-                Lowest:
-                {cryptoInfo.market_data &&
-                  cryptoInfo.market_data.current_price.low_24h &&
-                  cryptoInfo.market_data.current_price.low_24h.eur}
-                €
-              </h6>
-              <h6>
-                Hightest:
-                {cryptoInfo.market_data &&
-                  cryptoInfo.market_data.current_price.high_24h &&
-                  cryptoInfo.market_data.current_price.high_24h.eur}
-                €
-              </h6>
+            <div className={styles.description}>
+              <h5> {cryptoInfo.description?.en}</h5>
             </div>
-          </div>
-
-          <div className={styles.supply}>
-            <h6>
-              Circulating supply:
-              {cryptoInfo.market_data &&
-                cryptoInfo.market_data.current_price.circulating_supply}
-            </h6>
-            <h6>
-              Total supply:
-              {cryptoInfo.market_data &&
-                cryptoInfo.market_data.current_price.total_supply}
-            </h6>
-          </div>
-
-          <div className={styles.marketCap}>
-            <h6>
-              Market Cap: €
-              {/* {cryptoInfo.market_data &&
-                cryptoInfo.market_data.current_price.market_cap &&
-                cryptoInfo.market_data.current_price.market_cap &&
-                `${cryptoInfo.market_data.current_price.market_cap.eur}`} */}
-              {cryptoInfo.market_data?.current_price?.market_cap?.eur}
-            </h6>
-          </div>
-        </div>
-        <div className={styles.description}>
-          <h5> {cryptoInfo.description?.en}</h5>
-        </div>
+          </>
+        )}
+        {console.log(cryptoInfo?.market_data?.current_price?.market_cap?.eur)}
 
         {isGlobalModal && (
           <GlobalModal
