b733d27ac979506befd8225d709704f50fe7dd4c
SoniaGrassia <grassiasonia@gmail.com>
2023-04-21 12:43:24 +0200

    popup visibility

---
+++ src/components/popup/Popup.jsx
@@ -0,0 +1,7 @@
+import Button from "../button";
+import styles from "./index.module.scss";
+
+const Popup = (prop) => {
+  return <div className={styles.Popup}>{prop.text}</div>;
+};
+export default Popup;
