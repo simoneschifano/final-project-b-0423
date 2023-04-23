0889ae7e9bc6e76eeab4c569bb757d94943283ea
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-04-23 16:20:47 +0200

    fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/pages/custom404.module.scss";
+import Link from "next/link";
 
 const Custom404 = () => {
   return (
@@ -13,9 +14,9 @@ const Custom404 = () => {
       <div className={styles.Custom404}>
         <h1>Error 404</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
-          <a className={styles.home404} href="/">
-            back to home page
-          </a>
+        <Link className={styles.home404} href="/">
+          back to home page
+        </Link>
       </div>
     </>
   );
